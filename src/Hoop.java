a3783540ed3f529b29ed1810b53d43fdc6d90ab1
Andy Russell <arussell123@gmail.com>
2012-10-21 02:54:35 -0700

    Fixed hoops changing speed while Charlie is mid-jump

--- src/Hoop.java
+++ src/Hoop.java
@@ -4,36 +4,44 @@ import objectdraw.*;
 
 public class Hoop extends ActiveObject {
     private static final int MOVE_DELAY = 20;
-    
-    private VisibleImage theRing;
+
+    private VisibleImage theHoop;
     private Charlie charlie;
-    private double baseSpeed;
-    
-    public Hoop (Image ringImage, Charlie charlie, Location loc, double speed, DrawingCanvas canvas) {
-        theRing = new VisibleImage(ringImage, loc, canvas);
+    private double baseSpeed, fasterSpeed, slowerSpeed;
+
+    public Hoop(Image ringImage, Charlie charlie, Location loc, double speed,
+            DrawingCanvas canvas) {
+        theHoop = new VisibleImage(ringImage, loc, canvas);
         this.baseSpeed = speed;
         this.charlie = charlie;
-                
+
+        fasterSpeed = speed + .2;
+        slowerSpeed = speed - .15;
+
         start();
     }
-    
+
     @Override
-    public void run () {
-        while (theRing.getX() + theRing.getWidth() > 0) {
-            double speed = baseSpeed;
-            
-            if (charlie.isMovingForward()) {
-                // Make speed faster
-                speed += .2;
-            } else if (charlie.isMovingBackward()) {
-                // Make speed slower
-                speed -= .15;
+    public void run() {
+        double speed = baseSpeed;
+
+        while (theHoop.getX() + theHoop.getWidth() > 0) {
+            if (!charlie.isJumping()) {
+                if (charlie.isMovingForward()) {
+                    // Make speed faster
+                    speed = fasterSpeed;
+                } else if (charlie.isMovingBackward()) {
+                    // Make speed slower
+                    speed = slowerSpeed;
+                } else {
+                    speed = baseSpeed;
+                }
             }
-            
+
             double moveDistance = speed * MOVE_DELAY;
-            
-            theRing.move(-moveDistance, 0);
-            if (charlie.overlaps(theRing)) {
+
+            theHoop.move(-moveDistance, 0);
+            if (charlie.overlaps(theHoop)) {
                 charlie.kill();
             }
             pause(MOVE_DELAY);
