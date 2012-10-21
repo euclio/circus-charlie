31349ac1861d54e7d985e56e11a7b800a7fa5013
Andy Russell <arussell123@gmail.com>
2012-10-20 21:39:01 -0700

    Fixed multiple key press bug

--- src/CircusCharlie.java
+++ src/CircusCharlie.java
@@ -10,20 +10,20 @@ public class CircusCharlie extends WindowController implements KeyListener {
 
     protected static final int CANVAS_HEIGHT = 600;
     protected static final int CANVAS_WIDTH = 800;
-    
+
     private final Set<Character> pressed = new HashSet<Character>();
 
     public void begin() {
-    	//making charlie and the background
+        // making charlie and the background
         background = new Background(getImage("resources/stupidBackground.jpg"),
                 canvas);
         charlieImage = getImage("resources/tempLion.jpg");
         charlie = new Charlie(charlieImage, background, new Location(0, 400),
                 canvas);
-        //set window size
+        // set window size
         this.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
 
-        //set up key listener
+        // set up key listener
         requestFocus();
         addKeyListener(this);
         canvas.addKeyListener(this);
@@ -36,47 +36,25 @@ public class CircusCharlie extends WindowController implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-    	System.out.println(e.getKeyCode());
-        // Checking for multiple key presses
-    	pressed.add(e.getKeyChar());
-    	//if the player jumps while moving, jump and keep moving
-        if (pressed.size()>1) {
-        	System.out.println("Multiple Keys Pressed");
-            if (pressed.contains(KeyEvent.VK_RIGHT) && e.getKeyCode() == KeyEvent.VK_SPACE) {
-            	charlie.jumpAndMoveForward();
-            }
-            if (pressed.contains(KeyEvent.VK_LEFT) && e.getKeyCode() == KeyEvent.VK_SPACE) {
-            	charlie.jumpAndMoveBackward();
-            }
-        }
-        //if the player is only pressing one key, move or jump appropriately
-        else{
-        	if (e.getKeyCode()==KeyEvent.VK_RIGHT){
-        		charlie.moveForward();
-        	}
-        	else if (e.getKeyCode()==KeyEvent.VK_LEFT){
-        		charlie.moveBackward();
-        	}
-        	else if (e.getKeyCode()==KeyEvent.VK_SPACE){
-        		charlie.jump();
-        	}
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            charlie.moveForward();
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            charlie.moveBackward();
         }
 
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            charlie.jump();
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-    	System.out.println(e.getKeyCode());
         // stop moving if you were moving
-        pressed.remove(e.getKeyChar());
-        if(e.getKeyCode()==(KeyEvent.VK_LEFT)||e.getKeyCode()==(KeyEvent.VK_RIGHT)){
-        charlie.stopMoving();
-    }
-        //stop jumping if you were jumping
-        else if (e.getKeyCode() == KeyEvent.VK_SPACE){
-        	charlie.stopJumping();
+        if (e.getKeyCode() == KeyEvent.VK_LEFT
+                || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            charlie.stopMoving();
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            charlie.stopJumping();
         }
-
-
-}
+    }
 }
